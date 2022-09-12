40bc80eaca8cc68cca547463430a20aa83315e9a
Иван <ivankiseliov@rambler.ru>
2022-09-13 01:18:16 +0300

    задание 5

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,30 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int year = 2024;
-        boolean hiYear = (year % 4) == 0 || (year % 100) == 0;
-        if (hiYear) {
-            System.out.println("Год високосный");
-        } else {System.out.println("Год не високосный");
+       int monthNumber = 12;
+       switch (monthNumber) {
+           case 12:
+           case 1:
+           case 2:
+               System.out.println("Зима");
+               break;
+           case 3:
+           case 4:
+           case 5:
+               System.out.println("Весна");
+               break;
+           case 6:
+           case 7:
+           case 8:
+               System.out.println("Лето");
+               break;
+           case 9:
+           case 10:
+           case 11:
+               System.out.println("Осень");
+               break;
+           default:
+               System.out.println("Нет такого месяца");
         }
         }
         }

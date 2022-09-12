2210588eb690fce98dfde4a01ff98d2fca7bc591
Иван <ivankiseliov@rambler.ru>
2022-09-13 01:09:40 +0300

    задание 5

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,22 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int year = 2024;
-        boolean hiYear = (year % 4) == 0 || (year % 100) == 0;
-        if (hiYear) {
-            System.out.println("Год високосный");
-        } else {System.out.println("Год не високосный");
-        }
-        }
-        }
-
+      int deliveryDistance = 95;
+      int zoneOne = 20;
+      int zoneTwo = 60;
+      int zoneThree = 100;
+      boolean dayThree = (deliveryDistance < zoneThree && deliveryDistance > zoneTwo);
+      boolean dayTwo = (deliveryDistance > zoneOne && deliveryDistance < zoneTwo);
+      boolean dayOne = (deliveryDistance < zoneOne);
+      boolean dayMore = (deliveryDistance > zoneThree);
+      if (dayThree) {
+          System.out.println("Потребуется дней: 3");
+      } else if (dayTwo) {
+              System.out.println("Потребуется дней: 2");
+          } else if (dayOne) {
+          System.out.println("Потребуется дней: 1");
+      } else if (dayMore) {
+          System.out.println("Доставка от 3 дней");
+      }
+      }
+}

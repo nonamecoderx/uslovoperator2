7e7b0a983b8dce62b3090ea2efd9b369ec8a1f0e
Иван <ivankiseliov@rambler.ru>
2022-09-13 00:49:21 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,12 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int date = 2008;
-        int clientOS = 1;
-        if (clientOS == 0 && date >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 0 && date < 2015){
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        int year = 2024;
+        boolean hiYear = (year % 4) == 0 || (year % 100) == 0;
+        if (hiYear) {
+            System.out.println("Год високосный");
+        } else {System.out.println("Год не високосный");
         }
-        if (clientOS == 1 && date >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 1 && date < 2015){
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
-    }
-    }
+        }
+

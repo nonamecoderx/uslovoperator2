be6113255c19543625a9f3b68014a68a0ec036ef
Иван <ivankiseliov@rambler.ru>
2022-09-13 00:20:58 +0300

    задание 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int clientOS = 0;
+                if (clientOS == 0) {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+                else if (clientOS ==1) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+    }
+}
